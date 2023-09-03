5177374780478cca365170f1e7104de7f9588594
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2023-09-03 16:28:16 +0530

    changes

---
+++ components/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    context: __dirname + "/src",
+    entry: './index.js',
+  
+    output: {
+      filename: "index.js",
+      path: __dirname + '/dist'
+    },
+  
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: ["react-hot-loader", "babel-loader"],
+        }
+      ],
+      rules: [
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+    },
+  }
